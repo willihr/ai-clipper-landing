9408da2cf27ddab6d971e1cd7fbdfdab38870bcb
willihr <willihr10@gmail.com>
2025-09-01 23:17:25 +0000

    feat: initial template files

---
+++ app/release-notes/page.js
@@ -0,0 +1,18 @@
+import BackToTop from "../backToTop";
+import ReleaseNotesPage from "./index";
+
+export const metadata = {
+  title: "Release Notes - || AiWave - AI SaaS Website NEXTJS14 UI Kit",
+  description: "AiWave - AI SaaS Website NEXTJS14 UI Kit",
+};
+
+const ReleaseNotesLayout = () => {
+  return (
+    <>
+      <ReleaseNotesPage />
+      <BackToTop />
+    </>
+  );
+};
+
+export default ReleaseNotesLayout;
