152d5ef2ac729017c303f28696ea963cf450fd29
willihr <willihr10@gmail.com>
2025-09-09 16:46:56 +0000

    fix: html title and description

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import BackToTop from "./backToTop";
 import HomePage from "./home/page";
 
 export const metadata = {
-  title: "Home - || AiWave - AI SaaS Website NEXTJS14 UI Kit",
-  description: "AiWave - AI SaaS Website NEXTJS14 UI Kit",
+  title: "Transforme Vídeos em Cortes Virais para Redes Sociais | ViroClip",
+  description: "Economize tempo e maximize seu alcance! A IA da ViroClip analisa seus vídeos e cria dezenas de cortes virais, prontos para postar no TikTok, Reels e Shorts. Aumente seu engajamento. Teste grátis!",
 };
 
 export default function Home() {
