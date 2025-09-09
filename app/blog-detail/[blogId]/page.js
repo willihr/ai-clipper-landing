880c2cb2b29e39d243d05cb9cf71be70998222b8
willihr <willihr10@gmail.com>
2025-09-09 18:53:53 +0000

    fix: static generation

--- app/blog-detail/[blogId]/page.js
+++ app/blog-detail/[blogId]/page.js
@@ -1,12 +1,19 @@
 import BackToTop from "@/app/backToTop";
 import BlogDetailsPage from "../index";
-
-export const runtime = "edge";
+import BlogData from "../../../data/blog.json";
 
 export const metadata = {
   title: "Blog Details - || AiWave - AI SaaS Website NEXTJS14 UI Kit",
   description: "AiWave - AI SaaS Website NEXTJS14 UI Kit",
 };
+
+export async function generateStaticParams() {
+  let blogPosts = JSON.parse(JSON.stringify(BlogData.blog));
+ 
+  return blogPosts.map((post) => ({
+    blogId: post.id.toString(),
+  }))
+}
 
 const BlogDetailsLayout = ({ params }) => {
   return (
